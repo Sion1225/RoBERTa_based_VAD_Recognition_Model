8bfb398a005764fb905c5a3948a9af793ff1d266
Siwon <visiopo44@naver.com>
2023-06-28 23:13:07 +0900

    add min on learning scheduler

--- RoBERTa_Learning_scheduler.py
+++ RoBERTa_Learning_scheduler.py
@@ -7,8 +7,9 @@ RoBERTa: A Robustly Optimized BERT Pretraining Approach (Yinhan Liu, Myle Ott et
 import tensorflow as tf
 
 class Linear_schedule_with_warmup(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, max_lr: int, num_warmup: int , num_traning: int):
+    def __init__(self, max_lr: float, min_lr:float, num_warmup: int , num_traning: int):
         self.max_lr = max_lr
+        self.min_lr = min_lr # RoBERTa: 0.0
         self.num_warmup = num_warmup
         self.num_training = num_traning
 
@@ -16,12 +17,13 @@ class Linear_schedule_with_warmup(tf.keras.optimizers.schedules.LearningRateSche
         return tf.cond(
             step_now < self.num_warmup,
             lambda: (tf.cast(step_now, tf.float32) / tf.cast(tf.maximum(1.0, tf.cast(self.num_warmup, tf.float32)), tf.float32)) * self.max_lr,
-            lambda: tf.maximum(0.0, self.max_lr * ((tf.cast(self.num_training, tf.float32) - tf.cast(step_now, tf.float32)) / tf.cast(tf.maximum(1.0, tf.cast(self.num_training - self.num_warmup, tf.float32)), tf.float32)))
+            lambda: tf.maximum(self.min_lr, self.max_lr * ((tf.cast(self.num_training, tf.float32) - tf.cast(step_now, tf.float32)) / tf.cast(tf.maximum(1.0, tf.cast(self.num_training - self.num_warmup, tf.float32)), tf.float32)))
             )
     
     def get_config(self) -> dict:
         return {
             "max_lr": self.max_lr,
+            "min_lr": self.min_lr,
             "num_warmup": self.num_warmup,
             "num_training": self.num_training
             }
--- VAD_Assinging_RoBERTa_model.py
+++ VAD_Assinging_RoBERTa_model.py
@@ -122,11 +122,14 @@ ES = tf.keras.callbacks.EarlyStopping(monitor="val_mse", mode="max", patience=4,
 
 # load defined model and compile
 model = TF_RoBERTa_VAD_Classification("roberta-base")
+
 num_training = ceil((len(X_train[0])/model_H_param.num_batch_size))
 warmup_ratio = 0.048 # <<< Hyper-parameter; RoBERTa's: 4.8% (0.048)
-lr_schedule = Linear_schedule_with_warmup(max_lr=1e-1, num_warmup=round(num_training*warmup_ratio), num_traning=num_training) # RoBERTa's max_lr: 6e-4, num_training: Number of all backpropagation <<<<<< Hyper parameter
-optimizer = tf.keras.optimizers.experimental.AdamW(learning_rate=lr_schedule, beta_1=0.9, beta_2=0.999, epsilon=1e-7, weight_decay=0.01) # In the RoBERTa; beta_2=0.98, epsilon=1e-6, weight_decay=0.01 <<<<<< Hyper parameter
+lr_schedule = Linear_schedule_with_warmup(max_lr=5e-2, min_lr=0.0, num_warmup=round(num_training*warmup_ratio), num_traning=num_training) # RoBERTa's max_lr: 6e-4, num_training: Number of all backpropagation <<<<<< Hyper parameter
+optimizer = tf.keras.optimizers.experimental.AdamW(learning_rate=lr_schedule, beta_1=0.9, beta_2=0.999, epsilon=1e-7, weight_decay=0.0) # In the RoBERTa; beta_2=0.98, epsilon=1e-6, weight_decay=0.01 <<<<<< Hyper parameter
+
 loss = tf.keras.losses.MeanSquaredError()
+
 model.compile(optimizer=optimizer, loss=loss, metrics = ['mse'])
 model.fit(X_train, y_train, epochs=model_H_param.num_epochs, batch_size=model_H_param.num_batch_size, validation_data=(X_test, y_test), callbacks=[TensorB, ES])
 
